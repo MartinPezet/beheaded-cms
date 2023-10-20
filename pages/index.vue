637163e255127185951039b7a6bb921cf5b43246
Martin Pezet <mpezet@ntlworld.com>
2023-10-20 21:34:31 +0100

    Add vitest and Nuxt Testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h1>Hello world</h1>
+        <button @click="addCount(1)">Count: {{ count }}</button>
     </div>
 </template>
+
+
+
+<script setup lang="ts">
+let count = ref(0);
+
+function addCount(amount: number) {
+    count.value += amount;
+}
+</script>
